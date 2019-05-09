3d7aaad93825ddc89823d8fdfec2ddb07332b4e7
Sam Jimenez <sjimenez2@elon.edu>
2019-05-09 12:11:38 -0400

    Random start location within the upper left quadrant

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var p1, height, scale, board;
 function setup() {
 	boardDimensions();
   createCanvas(height, height);
-	p1 = new Player(scale, height, 100, 100);
+	startLocations();
 }
 
 function boardDimensions() {
@@ -11,6 +11,12 @@ function boardDimensions() {
   boarder = height * 0.025
   board = height - boarder - boarder;
   scale = board / 30;
+}
+
+function startLocations() {
+  var startX = Math.floor(Math.random() * 14) + 1;
+  var startY = Math.floor(Math.random() * 14) + 1;
+  p1 = new Player(scale, board, boarder + scale * startX, boarder + scale * startY);
 }
 
 function draw() {
