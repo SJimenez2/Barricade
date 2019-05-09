96c848c0f65065a2bff41e149bda2f394c54332a
Sam Jimenez <sjimenez2@elon.edu>
2019-05-09 12:46:26 -0400

    Added css and fixed spelling mistake

--- player.js
+++ player.js
@@ -5,14 +5,14 @@ class Player {
   score = 0;
   dead = false;
   total = 0;
-  baricade = [];
+  barricade = [];
 
   constructor(scale, height, startX, startY) {
     this.height = height;
     this.scale = scale;
     this.x = startX;
     this.y = startY;
-    this.baricade[this.total++] = createVector(startX, startY);
+    this.barricade[this.total++] = createVector(startX, startY);
   }
 
   getScore() {
@@ -26,20 +26,20 @@ class Player {
   update() {
     if (!this.dead && (this.moveX != 0 || this.moveY != 0)) {
       this.total++;
-      this.baricade[this.baricade.length] = createVector(this.x, this.y);
+      this.barricade[this.barricade.length] = createVector(this.x, this.y);
       this.x = this.x + this.moveX * this.scale;
       this.y = this.y + this.moveY * this.scale;
     }
   }
 
   show() {
-    for (var i = 0; i < this.baricade.length; i++) {
-      if (i == this.baricade.length - 1 && this.dead) {
+    for (var i = 0; i < this.barricade.length; i++) {
+      if (i == this.barricade.length - 1 && this.dead) {
         fill(255, 0, 0);
       } else {
         fill(255);
       }
-      rect(this.baricade[i].x, this.baricade[i].y, this.scale, this.scale);
+      rect(this.barricade[i].x, this.barricade[i].y, this.scale, this.scale);
     }
   }
 
@@ -54,7 +54,7 @@ class Player {
 
   kill() {
     if (this.x > this.height || this.y > this.height || this.x < 1 || this.y < 1) {
-      this.baricade[this.baricade.length] = createVector(this.x, this.y);
+      this.barricade[this.barricade.length] = createVector(this.x, this.y);
       this.dead = true;
       this.direction(0, 0);
       return true;
