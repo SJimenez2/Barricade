db223a67b1e7d246721ceb084a6c8b2ba6b96bde
Sam Jimenez <sjimenez2@elon.edu>
2019-05-09 12:34:19 -0400

    Added player 2 controlled by arrow keys

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var p1, height, scale, board;
+var p1, p2, height, scale, board;
 var restart = true;
 
 function setup() {
@@ -19,6 +19,7 @@ function startLocations() {
   var startX = Math.floor(Math.random() * 14) + 1;
   var startY = Math.floor(Math.random() * 14) + 1;
   p1 = new Player(scale, board, boarder + scale * startX, boarder + scale * startY);
+  p2 = new Player(scale, board, boarder + scale * (30 - startX), boarder + scale * (30 - startY));
 }
 
 function draw() {
@@ -30,26 +31,30 @@ function visuals() {
   background(0);
   fill(51);
   rect(height * 0.025, height * 0.025, board, board);
-	if (!p1.isdead()) {
+  if (!p1.isdead() && !p2.isdead()) {
+    p1.kill();
     p1.update();
-		p1.kill();
+    p2.kill();
+    p2.update();
   }
   p1.show();
+	p2.show();
   setTimeout(visuals, 200);
 }
 
 function keyPressed() {
-	// press a key to start game
-	if (restart) {
+  // press a key to start game
+  if (restart) {
     restart = false;
     p1.direction(0, 1);
+		p2.direction(0, -1);
     return;
   }
 
   // r key to restart
   if (keyCode === 82) {
     startLocations();
-		restart = true;
+    restart = true;
   }
 
   // player 1
@@ -61,5 +66,16 @@ function keyPressed() {
     p1.direction(1, 0);
   } else if (keyCode === 65) {
     p1.direction(-1, 0);
+  }
+
+	// player 2
+  if (keyCode === UP_ARROW) {
+    p2.direction(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    p2.direction(0, 1);
+  } else if (keyCode === RIGHT_ARROW) {
+    p2.direction(1, 0);
+  } else if (keyCode === LEFT_ARROW) {
+    p2.direction(-1, 0);
   }
 }
